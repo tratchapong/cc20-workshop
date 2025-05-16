0e9190c44746f1b200563b1124be2ed03bdc2e62
tratchapong <tratchapong@gmail.com>
2025-05-16 09:20:33 +0700

    starter with daisyui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="app">
+      <h1 className="text-3xl text-amber-300">Codecamp20</h1>
+      <button className="btn btn-primary">OK</button>
+    </div>
+  )
+}
+
+export default App
