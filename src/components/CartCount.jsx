d106a8ad4737f282004ba6bec5addcf3b1012c8f
tratchapong <tratchapong@gmail.com>
2025-05-18 22:50:39 +0700

    Header logo + cart : done

--- src/components/CartCount.jsx
+++ src/components/CartCount.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function CartCount(props) {
-  const {itemCount} = props
+  const { itemCount } = props
   return (
-    <div>CartCount : {itemCount}</div>
+    <div className='relative'>
+      <div className="w-16">
+        <img src="https://www.svgrepo.com/show/134537/cart.svg" alt="Cart" />
+      </div>
+      <div className='text-white bg-lime-500 w-6 h-6 rounded-full flex justify-center items-center absolute top-1/4 left-2/5'>{itemCount}</div>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import CartCount from './CartCount'
 function Header(props) {
   const {itemCount} = props
   return (
-    <div className='flex justify-between h-1/12 bg-amber-300'>
-      <div>
-        Logo, Brand
+    <div className='flex justify-between items-center px-4 h-1/12 bg-amber-300'>
+      <div className='w-15 h-15 flex items-start gap-2 border border-slate-300 bg-violet-300 rounded ps-1'>
+        <p className="text-xl text-white">CC</p>
+        <img src="https://www.svgrepo.com/show/401387/department-store.svg" alt="shop logo" />
       </div>
       <CartCount itemCount={itemCount}/>
     </div>
